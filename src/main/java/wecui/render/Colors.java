034242f2b8845b2eaf8c38775b7274d9d8e1b251
Yetanotherx <yetanotherx@gmail.com>
2011-12-18 20:41:16 -0500

    Fix line color issues in the grid.

--- src/main/java/wecui/render/Colors.java
+++ src/main/java/wecui/render/Colors.java
@@ -16,8 +16,8 @@ public class Colors {
     public static final LineInfo secondhidden = new LineInfo(3.0f, 0.2f, 0.2f, 0.8f, 0.2f, GL11.GL_GEQUAL);
     public static final LineInfo polynormal = new LineInfo(3.0f, 0.2f, 0.8f, 0.8f, 0.8f, GL11.GL_LESS);
     public static final LineInfo polyhidden = new LineInfo(3.0f, 0.2f, 0.8f, 0.8f, 0.2f, GL11.GL_GEQUAL);
-    public static final LineInfo gridnormal = new LineInfo(3.0f, 0.8F, 0.2F, 0.2F, 0.2f, GL11.GL_LESS);
-    public static final LineInfo gridhidden = new LineInfo(2.0f, 0.4F, 0.1F, 0.1F, 0.1f, GL11.GL_GEQUAL);
+    public static final LineInfo gridnormal = new LineInfo(3.0f, 0.8F, 0.2F, 0.2F, 0.4f, GL11.GL_LESS);
+    public static final LineInfo gridhidden = new LineInfo(3.0f, 0.8F, 0.2F, 0.2F, 0.2f, GL11.GL_GEQUAL);
     public static final LineInfo boxnormal = new LineInfo(3.0f, 0.8F, 0.2F, 0.2F, 1.0f, GL11.GL_LESS);
     public static final LineInfo boxhidden = new LineInfo(2.0f, 0.4F, 0.1F, 0.1F, 0.2f, GL11.GL_GEQUAL);
 }
