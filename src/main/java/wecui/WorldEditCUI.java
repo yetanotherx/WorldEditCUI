c6b3838851f10233fac1e99fc6c4d45795479eb0
seannie2 <amazingcoolawesomestuff@gmail.com>
2012-05-30 21:21:26 -0300

    Update src/main/java/wecui/WorldEditCUI.java

--- src/main/java/wecui/WorldEditCUI.java
+++ src/main/java/wecui/WorldEditCUI.java
@@ -36,8 +36,8 @@ import wecui.render.region.CuboidRegion;
  */
 public class WorldEditCUI {
 
-    public static final String VERSION = "1.2";
-    public static final String MCVERSION = "1.2";
+    public static final String VERSION = "1.2.5";
+    public static final String MCVERSION = "1.2.5";
     public static final int protocolVersion = 1;
     protected Minecraft minecraft;
     protected EventManager eventManager;
